27a1871529c75af00ca991fd0b2e2495a3732fee
jyoungl <118170196+leejun002@users.noreply.github.com>
2022-12-24 10:33:46 +0900

    Update login_server.js

--- login_server.js
+++ login_server.js
@@ -14,3 +14,36 @@ loginButton.addEventListener("click", (e) => {
         document.getElementById('error-msg-holder').style.display="block";
     }
 })
+
+//save data from registration-form-submit
+const registerForm = document.getElementById("register-form");
+const registerButton = document.getElementById("register-form-submit");
+const registerErrorMsg = document.getElementById("register-error-msg");
+
+
+function send_data() {
+    var id = document.getElementById("id").value;
+    var email = document.getElementById("email").value;
+    var psw = document.getElementById("psw").value;
+    var conf_psw = document.getElementById("conf_psw").value;
+    var data = {
+        "id": id,
+        "email": email,
+        "psw": psw,
+        "conf_psw": conf_psw
+    };
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:3000/registration", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var response = JSON.parse(xhr.responseText);
+            if (response.success) {
+                alert("Registration successful");
+            } else {
+                alert("Registration failed");
+            }
+        }
+    }
+}
