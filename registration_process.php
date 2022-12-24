f9d8a8d68eb27d22ed5fe9ad0d94b63ab40ba13b
jyoungl <118170196+leejun002@users.noreply.github.com>
2022-12-24 14:34:45 +0900

    Update registration_process.php

--- registration_process.php
+++ registration_process.php
@@ -8,23 +8,25 @@ if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
     die("Valid email is required");
 }
 
-if (strlen($_POST["psw"]) < 8) {
+if (strlen($_POST["password"]) < 8) {
     die("Password must be at least 8 characters");
 }
 
-if (! preg_match("/[a-z]/i", $_POST["psw"])) {
+if (! preg_match("/[a-z]/i", $_POST["password"])) {
     die("Password must contain at least one letter");
 }
 
-if (! preg_match("/[0-9]/", $_POST["psw"])) {
+if (! preg_match("/[0-9]/", $_POST["password"])) {
     die("Password must contain at least one number");
 }
 
-if ($_POST["psw"] != $_POST["conf_psw"]) {
+if ($_POST["password"] != $_POST["confirm_password"]) {
     die("Passwords must match");
 }
 
-$password_hash = password_hash($_POST["psw"], PASSWORD_DEFAULT);
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 print_r($_POST);
 var_dump($password_hash);
+
+?>
