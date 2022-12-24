a77555cb1c6f2d3509b5ac425f0722ef031915c2
jyoungl <118170196+leejun002@users.noreply.github.com>
2022-12-24 20:03:01 +0900

    Create login.php

---
+++ registration/login.php
@@ -0,0 +1,141 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+form {border: 3px solid #f1f1f1;}
+
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+button {
+  background-color: #222222;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+.closecontainer {
+  margin: 5px 0 10px 0;
+  position: relative;
+}
+
+.container {
+  padding: 25px;
+}
+
+.msg-holder {
+}
+
+p.msg {
+  font-size: 15px;
+  color: red;
+}
+
+#error-msg-holder {
+  display: none;
+}
+
+.bottomcontainer {
+  padding: 20px 30px 35px 30px;
+}
+
+span.membership {
+  float: left;
+}
+
+span.psw {
+  float: right;
+}
+
+/* The Modal (background) */
+.modal {
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.2); /* Black w/ opacity */
+  padding-top: 60px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+  border: 1px solid #a0a0a0;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+
+.close {
+  position: absolute;
+  right: 15px;
+  top: 3px;
+  color: #000;
+  font-size: 35px;
+  font-weight: bold;
+}
+
+.close:hover,.close:focus {
+  opacity: 0.8;
+  cursor: pointer;
+}
+
+@media screen and (max-width: 300px) {
+  span.psw {
+     display: block;
+     float: none;
+  }
+}
+</style>
+<script src="login_server.js"></script>
+<script src="https://kit.fontawesome.com/1a2cc02835.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<div class="modal">
+  <form method="post" action="login.php" class="modal-content">
+    <div class="closecontainer">
+      <span onclick="location.href='mainpage.html'" class="close" title="Close Modal">&times;</span>
+    </div>
+    <?php include('errors.php'); ?>
+    <div class="container">
+      <input type="text" placeholder="ID" name="id" required>
+      <input type="password" placeholder="Password" name="psw" required>
+      <div class="msg-holder" id="error-msg-holder">
+        <p class="msg" id="error-msg"><i class="fa-solid fa-exclamation"></i> Invalid username <span id="error-msg-second-line">and/or password</span></p>
+      </div>
+      <button type="submit" id="login-form-submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me </input>
+      </label>
+    </div>
+    <div class="bottomcontainer" style="background-color:#f1f1f1">
+      <span class="membership"><a href="registration_form.html">Create yours.</a></span>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+  </form>
+</div>
+
+
+
+</body>
+</html>
