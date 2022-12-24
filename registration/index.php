7e0719b07e305559e97f2c88ecce4499c6aa09a0
jyoungl <118170196+leejun002@users.noreply.github.com>
2022-12-24 20:07:32 +0900

    Update index.php

--- registration/index.php
+++ registration/index.php
@@ -1,13 +1,13 @@
 <?php 
   session_start(); 
 
-  if (!isset($_SESSION['username'])) {
+  if (!isset($_SESSION['id'])) {
   	$_SESSION['msg'] = "You must log in first";
   	header('location: login.php');
   }
   if (isset($_GET['logout'])) {
   	session_destroy();
-  	unset($_SESSION['username']);
+  	unset($_SESSION['id']);
   	header("location: login.php");
   }
 ?>
@@ -36,8 +36,8 @@
   	<?php endif ?>
 
     <!-- logged in user information -->
-    <?php  if (isset($_SESSION['username'])) : ?>
-    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    <?php  if (isset($_SESSION['id'])) : ?>
+    	<p>Welcome <strong><?php echo $_SESSION['id']; ?></strong></p>
     	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
     <?php endif ?>
 </div>
